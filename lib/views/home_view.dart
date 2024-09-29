71c1d6f46c99ae62c5a0a3eaffb3c015c2701602
ahmed wael <ahmed855wael@gmail.com>
2024-09-30 01:19:29 +0300

    make a constant file , and app bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/constants.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Recipe',
+              style: TextStyle(
+                  color: kcolor, fontWeight: FontWeight.bold, fontSize: 26),
+            ),
+            Text(
+              'App',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+      body: const Column(
+        children: [Text(' Hey Ahmed ,Good Morning')],
+      ),
+    );
   }
 }
